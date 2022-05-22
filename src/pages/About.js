d9efed70f0e3a0453af4f5bc1b0b46d1f5299840
anndygeo <isar_anndy@yahoo.com>
2022-05-22 05:30:52 +0300

    add file

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About Us</h1>
+      <p>
+        lorem40 lorem40 lorem40 lorem40 lorem40 lorem40 
+        lorem40 lorem40 lorem40 lorem40 lorem40 
+        lorem40 lorem40 lorem40 lorem40 lorem40 lorem40 lorem40 lorem40 lorem40 
+        lorem40 lorem40 lorem40 lorem40 lorem40 lorem40 lorem40 lorem40 lorem40 lorem40 lorem40 
+        lorem40 lorem40 lorem40 lorem40 lorem40 lorem40 lorem40 lorem40 
+      </p>
+    </section>
+  )
+}
+
+export default About
